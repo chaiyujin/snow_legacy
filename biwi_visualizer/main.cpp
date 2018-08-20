c1d77e1049e35299ec5ad1fa73ddd3c8817fea0d
chaiyujin <chaiyujin@gmail.com>
2018-08-20 20:31:49 +0800

    nothin

--- biwi_visualizer/main.cpp
+++ biwi_visualizer/main.cpp
@@ -2,48 +2,29 @@
 #include <string>
 #include "window.h"
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <thread>
 
 snow::App *app = nullptr;
+std::map<std::string, std::string> objMap;
 
-void run(std::string obj_path) {
-    bool stop = false;
+void run(std::vector<std::pair<std::string, std::string>> win_obj_pairs) {
     app = new App();
     auto t = std::thread([&]() -> void {
-        auto *win = new ObjWindow("obj viewer");
-        app->addWindow(win);
+        for (auto pair : win_obj_pairs) {
+            auto *win = new ObjWindow(pair.first.c_str());
+            win->loadObj(pair.second.c_str());
+            app->addWindow(win);
+        }
         app->run();
-        printf("quit\n");
         delete app;
         app = nullptr;
     });
-    t.detach();
-    while (!stop) {
-        auto *p = app->getWindow("obj viewer");
-        if (p) {
-            ((ObjWindow*)p)->loadObj(obj_path);
-            break;
-        }
-    }
-}
-
-void stop() {
-    if (app) {
-        auto *p = app->getWindow("obj viewer");
-        if (p) {
-            app->quit();
-        }
-    }
-}
-
-int add(int i, int j) {
-    return i + j;
+    t.join();
 }
 
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     m.def("run", &run, "");
-    m.def("stop", &stop, "");
-    m.def("add", &add, "");
 }
