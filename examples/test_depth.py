dd0d64a74bda834c842f0c1379d5f814194d8039
chaiyujin <chaiyujin@gmail.com>
2018-09-27 14:27:25 +0800

    bilateral filter

--- examples/test_depth.py
+++ examples/test_depth.py
@@ -1,5 +1,6 @@
 from anime_viewer import anime_viewer11
 from depth_video import depth11
+from bilateral_filter import BilateralFilter1D
 
 depth11.set_fps(40.0)
 depth11.set_sample_rate(20480)
@@ -7,6 +8,10 @@ result = depth11.collect_video("../assets/test_depth/0-0-1.mkv")
 if result is None:
     print("error")
 else:
+    filter = BilateralFilter1D(
+        factor=-0.5, distance_sigma=5,
+        range_sigma=1, radius=1)
+    result["expr_frames"] = filter.filter(result["expr_frames"])
     anime_viewer11.initialize_bilinear("../assets/fw/")
     anime_viewer11.new_app("bilinear")
     anime_viewer11.set_iden("win", result["iden"])
