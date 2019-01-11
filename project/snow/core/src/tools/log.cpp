796c0fe81ec3d6ac035a2d44f6c4d89c4402c225
chaiyujin <chaiyujin@gmail.com>
2019-01-11 22:56:56 +0800

    fix a typo

--- project/snow/core/src/tools/log.cpp
+++ project/snow/core/src/tools/log.cpp
@@ -5,7 +5,7 @@ namespace snow { namespace log {
 static bool gLevelSet = false;
 static std::vector<std::shared_ptr<spdlog::logger>> gLoggers;
 
-std::vector<std::shared_ptr<spdlog::logger>> &GetLoggers() {
+std::vector<std::shared_ptr<spdlog::logger>> &get_loggers() {
     if (!gLevelSet) {
         spdlog::set_level(__RAY_LOG_LEVEL__);
         gLevelSet = true;
